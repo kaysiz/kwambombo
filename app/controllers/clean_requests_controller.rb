45f47aefa201e7db8cf6dce570ac74fe9516afbf
Kudakwashe Siziva <ksiziva@gmail.com>
2019-07-05 07:29:11 +0200

    gemset

--- app/controllers/clean_requests_controller.rb
+++ app/controllers/clean_requests_controller.rb
@@ -1,4 +1,5 @@
 class CleanRequestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_clean_request, only: [:show, :edit, :update, :destroy]
 
   # GET /clean_requests
