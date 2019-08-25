bd7a76fdd46b09d2ebf03556c0a1813fceaa05a6
Kudakwashe Siziva <ksiziva@gmail.com>
2019-08-25 13:27:17 +0200

    updated production file to have default_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'www.kwambombo19.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'kwambombo19.heroku.com' }
 
 end
