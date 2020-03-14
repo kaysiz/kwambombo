19e3623b05bc31f34ea89ab960e160f8788e9657
Kudakwashe Siziva <ksiziva@gmail.com>
2020-03-14 16:41:49 +0200

    fix timeout flash messages

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -5,6 +5,7 @@ module FlashHelper
       next if message.blank?
       type = :success if type.to_sym == :notice
       type = :danger if type.to_sym == :alert
+      type = :danger if type.to_sym == :timedout
       Array(message).each do |msg|
         text = content_tag(:div, msg.html_safe, :class => "alert alert-#{type} rounded-0 mb-0")
         flash_messages << text if msg
