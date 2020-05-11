b4aa586e27bc3d8d22bd1e42a6cb448a39281c2f
Kudakwashe Siziva <ksiziva@gmail.com>
2020-05-11 02:03:21 +0200

    masks page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :users
   resources :payments
   get 'order/:id' => 'payments#order'
-  post 'payments/update_payment/:id' => 'payments#update_payment' 
+  post 'payments/update_payment/:id' => 'payments#update_payment'
+  get '/masks' => 'welcome#masks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
